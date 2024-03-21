9785cfe02c53b1984f5be65d26501331e82f73e3
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2024-03-22 00:20:27 +0100

    Feat: changed viz color | added another viz

--- app.py
+++ app.py
@@ -14,40 +14,80 @@ authenticate()
 
 st.set_page_config(layout="wide")
 
-st.title("Brazil Wildfire Analysis")
+st.header("Brazil Wildfire Analysis")
 
 
 brazil_shapefile = geemap.shp_to_ee('data/Brazil.shp')
 
-Map = geemap.Map()
+col1, col2, col3 = st.columns(3)
 
-# Define the dataset and filter by date
-dataset = ee.ImageCollection('MODIS/061/MCD64A1').filter(ee.Filter.date('2017-01-01', '2018-05-01'))
-burnedArea = dataset.select('BurnDate')
+with col1:
+    st.subheader("Burned Area")
+    Map = geemap.Map()
 
-# A FeatureCollection defining Brazil boundary.
-fc = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(
-    'country_na == "Brazil"'
-)
+    # Define the dataset and filter by date
+    dataset = ee.ImageCollection('MODIS/061/MCD64A1').filter(ee.Filter.date('2017-01-01', '2018-05-01'))
+    burnedArea = dataset.select('BurnDate')
 
-# Clip the burned Area by the Brazil boundary FeatureCollection.
-# Iterate over the ImageCollection and clip each image to the FeatureCollection.
-ba_clip = burnedArea.map(lambda img: img.clipToCollection(fc))
+    # A FeatureCollection defining Brazil boundary.
+    fc = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017').filter(
+        'country_na == "Brazil"'
+    )
+
+    # Clip the burned Area by the Brazil boundary FeatureCollection.
+    # Iterate over the ImageCollection and clip each image to the FeatureCollection.
+    ba_clip = burnedArea.map(lambda img: img.clipToCollection(fc))
 
 
-# Visualization parameters
-burnedAreaVis = {
-    'min': 30.0,
-    'max': 341.0,
-    'palette': ['4e0400', '951003', 'c61503', 'ff1901']
-}
+    # Visualization parameters
+    burnedAreaVis = {
+        'min': 30.0,
+        'max': 341.0,
+        'palette': ['4e0400', '951003', 'c61503', 'ff1901']
+    }
 
-# Create a map
-Map = geemap.Map(center=[-10, -55], zoom=4)
+    # Create a map
+    Map = geemap.Map(center=[-10, -55], zoom=4)
 
-# Add burned area layer to the map
-Map.addLayer(ba_clip, burnedAreaVis, 'Burned Area')
-Map.addLayer(brazil_shapefile, name='Brazil',opacity=0.5)
-Map.addLayerControl()
+    # Add burned area layer to the map
+    Map.addLayer(ba_clip, burnedAreaVis, 'Burned Area')
+    Map.addLayer(brazil_shapefile, name='Brazil',opacity=0.5)
+    Map.addLayerControl()
 
-Map.to_streamlit()
+    Map.to_streamlit()
+
+with col2:
+    st.subheader("Land Cover")
+    Map = geemap.Map()
+
+    landcover = ee.Image('MODIS/006/MCD12Q1/2004_01_01').select('LC_Type1')
+
+    igbpLandCoverVis = {
+        'min': 1.0,
+        'max': 17.0,
+        'palette': [
+            '05450a',
+            '086a10',
+            '54a708',
+            '78d203',
+            '009900',
+            'c6b044',
+            'dcd159',
+            'dade48',
+            'fbff13',
+            'b6ff05',
+            '27ff87',
+            'c24f44',
+            'a5a5a5',
+            'ff6d4c',
+            '69fff8',
+            'f9ffa4',
+            '1c0dff',
+        ],
+    }
+
+    brazil_lc = landcover.clip(brazil_shapefile)
+    Map.setCenter(-55, -10, 4)
+    Map.addLayer(brazil_lc, igbpLandCoverVis, 'MODIS Land Cover')
+    Map.addLayerControl()
+    Map.to_streamlit()
